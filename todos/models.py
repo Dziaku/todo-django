0cf269f8c5273c8e800d1bc4a122fc99fda1ba64
Dziaku <kkubik960@gmail.com>
2024-09-11 21:50:15 +0200

    Initial release of todos app. Contains semi-working models

---
+++ todos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDoList(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class ToDoItem(models.Model):
+    list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    item_description = models.CharField(max_length=256)
+    deadline_date = models.DateTimeField()
+    status = models.BooleanField(default= False)
+
+    def __str__(self):
+        return self.item_description
